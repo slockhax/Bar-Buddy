6528e251ec902729be40b6753b59d4d645648bee
stack-overflowed <scott.j.lockhart@gmail.com>
2023-12-24 03:10:58 -0800

    updated navigation, added M3 color scheme, added filters for recipes

--- app/src/main/java/com/example/barbuddy/MainActivity.kt
+++ app/src/main/java/com/example/barbuddy/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.barbuddy
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,17 +21,24 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -39,6 +46,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.barbuddy.ui.theme.AppTheme
 
 val Dao = DatabaseProvider.db.IngredientDao()
 val spiritsData = Dao.getSpirits()
@@ -52,7 +60,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScaffold()
+            AppTheme {
+                MainScaffold()
+            }
         }
     }
 }
@@ -62,44 +72,68 @@ class MainActivity : ComponentActivity() {
 fun MainScaffold() {
     val navController = rememberNavController()
 
+    val navColor = NavigationBarItemDefaults.colors(
+        selectedIconColor = MaterialTheme.colorScheme.primaryContainer,
+        selectedTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        unselectedIconColor =MaterialTheme.colorScheme.onSurface,
+        unselectedTextColor = MaterialTheme.colorScheme.onSurface,
+        indicatorColor = MaterialTheme.colorScheme.primary,
+    )
+
     Scaffold (
         topBar = {
             TopAppBar(
                  colors = TopAppBarDefaults.largeTopAppBarColors(
-                     containerColor = MaterialTheme.colorScheme.primaryContainer,
-                     titleContentColor = MaterialTheme.colorScheme.primary
+                     containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp),
+                     titleContentColor = MaterialTheme.colorScheme.onSurface
                  ),
                  title = { Text("Bar Buddy") }
             )
         },
         bottomBar = {
-            NavigationBar {
+            var selectedNavItem by remember { mutableIntStateOf(2) }
+            NavigationBar (
+                containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp),
+                contentColor = MaterialTheme.colorScheme.onSurface
+            ){
                 NavigationBarItem(
-                    selected = true,
-                    onClick = { navController.navigate("Ingredients") },
+                    selected = selectedNavItem == 0,
+                    onClick = {
+                        navController.navigate("Ingredients")
+                        selectedNavItem = 0
+                    },
                     label = { Text("Ingredients") },
                     icon = { Icon(
                         Icons.Rounded.ListAlt,
                         contentDescription = "test image")
-                    }
+                    },
+                    colors = navColor
                 )
                 NavigationBarItem(
-                    selected = true,
-                    onClick = { navController.navigate("Craftable") },
+                    selected = selectedNavItem == 1,
+                    onClick = {
+                        navController.navigate("Craftable")
+                        selectedNavItem = 1
+                    },
                     label = { Text("Craftable") },
                     icon = {
                         Icon(Icons.Rounded.Liquor,
                         contentDescription = "test image")
-                    }
+                    },
+                    colors = navColor
                 )
                 NavigationBarItem(
-                    selected = true,
-                    onClick = { navController.navigate("All Recipes")},
+                    selected = selectedNavItem == 2,
+                    onClick = {
+                        navController.navigate("All Recipes")
+                        selectedNavItem = 2
+                    },
                     label = { Text("All Recipes") },
                     icon = {
                         Icon(Icons.Rounded.Assignment,
                             contentDescription = "test image")
-                    }
+                    },
+                    colors = navColor
                 )
             }
         }
@@ -112,7 +146,7 @@ fun MainScaffold() {
                     bottom = paddingValues.calculateBottomPadding()
                 )
         ) {
-            NavHost(navController = navController, startDestination = "Ingredients"){
+            NavHost(navController = navController, startDestination = "All Recipes"){
                 composable("Ingredients"){
                     IngredientsBodyContent()
                 }
@@ -156,12 +190,21 @@ fun IngredientsBodyContent() {
 
 @Composable
 fun RecipesBodyContent(){
-    LazyColumn{
-        item {
-            for (recipe in allRecipes) {
-                CollapsibleCard(
-                    title = recipe.name,
-                    content = { RecipeContent(recipe.tags, recipe.ingredients) })
+    Column(){
+        BuildFilterRow()
+        LazyColumn{
+            allRecipes.chunked(2).forEach { chunk ->
+                item{
+                    Row(
+                        modifier = Modifier.padding(10.dp)
+                    ) {
+                        for (recipe in chunk) {
+                            RecipeCard(
+                                title = recipe.name,
+                                content = { RecipeContent(recipe.tags, recipe.ingredients) })
+                        }
+                    }
+                }
             }
         }
     }
@@ -170,16 +213,17 @@ fun RecipesBodyContent(){
 
 @Composable
 fun RecipeContent(tags: String?, ingredients: String){
-    Column{
-    Column{
+    Column (
+        modifier = Modifier.padding(10.dp)
+    ) {
     if (tags != null) {
-        Text("\nTags:\n")
-        Text(tags.replace(" ", "\n"))
+        val newTags = tags.replace(" ", "\n")
+        Text("\nTags:\n$newTags")
     }
         Text("\nIngredients:\n")
         Text(ingredients.replace(", ","\n"))
     }
-}}
+}
 
 @Composable
 fun BuildCheckboxGrid(ingredients: List<CocktailIngredients>){
@@ -221,8 +265,6 @@ fun BuildIndividualCheckbox(title: String, state: Boolean){
 
 fun updateCheck(title:String,value:Boolean){
     Dao.updateInventory(title, value.toString())
-    Log.e(title,value.toString())
-    Log.e(title,value.javaClass.name)
 }
 
 @Composable
@@ -233,7 +275,7 @@ fun CollapsibleCard(title: String, content: @Composable () -> Unit){
             .padding(5.dp),
         shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
         )
     ){
         Text(
@@ -245,4 +287,70 @@ fun CollapsibleCard(title: String, content: @Composable () -> Unit){
         )
         content()
     }
+}
+
+@Composable
+fun RowScope.RecipeCard(title: String, content: @Composable () -> Unit){
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .weight(1f)
+            .padding(5.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        )
+    ){
+        Text(
+            modifier = Modifier
+                .padding(start=20.dp,top=10.dp,bottom=5.dp)
+            ,
+            text = title,
+            fontSize = 20.sp
+        )
+        content()
+    }
+}
+
+val filters = listOf("Mixed","Shaken","Blended","Sweet","Fruity")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BuildFilterChip(name: String) {
+    var isSelected by remember { mutableStateOf(false) }
+
+    FilterChip(
+        colors = FilterChipDefaults.filterChipColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            labelColor = MaterialTheme.colorScheme.onBackground,
+            selectedContainerColor = MaterialTheme.colorScheme.tertiaryContainer,
+            selectedLabelColor = MaterialTheme.colorScheme.onTertiaryContainer
+//            iconColor = MaterialTheme.colorScheme.primary,
+        ),
+        selected = isSelected,
+        onClick = { isSelected = !isSelected },
+        label = { Text(name) },
+//        leadingIcon = {
+//            if (isSelected) {
+//                Icon(
+//                    Icons.Rounded.Done,
+//                    contentDescription = null,
+//                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+//                )
+//            }
+//        }
+    )
+}
+
+@Composable
+fun BuildFilterRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ){
+        for (filter in filters) {
+            BuildFilterChip(name = filter)
+        }
+    }
+
 }
