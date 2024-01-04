d7d80d23a6d57f95b8801e5c821005e528590e21
stack-overflowed <scott.j.lockhart@gmail.com>
2024-01-04 08:07:12 -0800

    added popup for filters, formatted but not functional.  there's a very annoying bug with the filter chips on popup

--- app/src/main/java/com/example/barbuddy/NavRecipes.kt
+++ app/src/main/java/com/example/barbuddy/NavRecipes.kt
@@ -1,17 +1,26 @@
 package com.example.barbuddy
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
@@ -24,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,12 +48,12 @@ fun RecipesBodyContent(navController: NavController){
         LazyColumn{
             item{
                 Dao.getAllRecipes().forEach { recipe ->
-                    val tags = collateTags(recipe)
+                    var tags = recipe.method
+                    recipe.descriptors?.let { tags += ", $it" }
                     Divider()
                     RecipeListItem(
                         navController,
                         itemName = recipe.name,
-                        method = recipe.method,
                         tags = tags
                     )
                 }
@@ -52,48 +62,100 @@ fun RecipesBodyContent(navController: NavController){
     }
 }
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BuildFilterChip(name: String, dropdown: Boolean = true) {
     val isSelected by remember { mutableStateOf(false) }
-    val showDialog by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
     FilterChip(
         colors = FilterChipDefaults.filterChipColors(
             containerColor = MaterialTheme.colorScheme.background,
             labelColor = MaterialTheme.colorScheme.onBackground,
-            selectedContainerColor = MaterialTheme.colorScheme.primaryContainer,
-            selectedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer
+            selectedContainerColor = MaterialTheme.colorScheme.background,
+            selectedLabelColor = MaterialTheme.colorScheme.onBackground
         ),
-        selected = isSelected,
-        onClick = { showDialog != showDialog },
+        selected = false,
+        onClick = { showDialog = !showDialog },
         label = { Text(name) },
         trailingIcon = { if (dropdown) Icon(Icons.Rounded.ArrowDropDown, contentDescription = null) }
     )
     if (showDialog) {
-        FilterPopup()
+//        FilterPopup2(name, onDismiss = {showDialog = false})
+        Text("")
     }
 }
 
-fun collateTags(recipe: Recipes): String{
-    var tags = recipe.method + ", "
-    if (recipe.boozy == 1) tags += "Boozy, "
-    if (recipe.citrusy == 1) tags += "Citrusy, "
-    if (recipe.frozen == 1) tags += "Frozen, "
-    if (recipe.fruity == 1) tags += "Fruity, "
-    if (recipe.sweet == 1) tags += "Sweet, "
-    if (recipe.tart == 1) tags += "Tart, "
-    if (recipe.warm == 1) tags += "Warm, "
-    tags = tags.dropLast(2)
-    return tags
-}
-
 @Composable
-fun FilterPopup(){
-    Popup (
-        alignment = Alignment.BottomStart
-    ){
-        Text("hello world")
+fun FilterPopup2(name: String = "Title", onDismiss: () -> Unit) {
+    Popup {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = { onDismiss() }
+            )
+            .background(Color.Black.copy(alpha = 0.5f))
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = {  }
+                )
+                .align(Alignment.BottomCenter),
+            shape = RoundedCornerShape(0.dp),
+            colors = CardDefaults.cardColors (
+                containerColor = Color.White,
+                contentColor = Color.Black,
+            ),
+        ) {
+            Text (
+                text = name,
+                modifier = Modifier
+                    .padding(top = 15.dp, start = 15.dp),
+                style = MaterialTheme.typography.titleLarge,
+            )
+            Card (
+                modifier = Modifier
+                    .padding(20.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color.LightGray,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+            ) {
+                val filters = listOf("Boozy","Citrusy","Frozen","Fruity","Sweet","Tart","Warm")
+
+                LazyColumn {
+                    items(filters) { filterItem ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(color = Color.White)
+                                .padding(3.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            val isChecked = remember { mutableStateOf(false) }
+                            Checkbox(
+                                checked = isChecked.value,
+                                onCheckedChange = { isChecked.value = !isChecked.value }
+                            )
+                            Text(
+                                modifier = Modifier.padding(start = 5.dp),
+                                text = filterItem
+                            )
+                        }
+                        Divider()
+                    }
+                }
+            }
+        }
+    }
     }
 }
 
@@ -102,7 +164,7 @@ fun clickRecipe(navController: NavController, name:String){
 }
 
 @Composable
-fun RecipeListItem(navController: NavController, itemName:String, method:String, tags: String){
+fun RecipeListItem(navController: NavController, itemName:String, tags: String){
     val onClickAction = { clickRecipe(navController, itemName) }
     val inStock = Dao.getRecipeByName(itemName).craftable == 1
     ListItem(
@@ -119,11 +181,12 @@ fun RecipeListItem(navController: NavController, itemName:String, method:String,
     )
 }
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun BuildFilterRow() {
-    FlowRow(
-        modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp),
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp, end = 10.dp),
         horizontalArrangement = Arrangement.SpaceEvenly
     ){
             BuildFilterChip("Filters")
